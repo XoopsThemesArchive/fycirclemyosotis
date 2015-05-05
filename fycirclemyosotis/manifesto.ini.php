593a2896ca675ff85ddf5f8c2422335390a59001
mambax7 <mambax7@gmail.com>
2015-05-05 09:03:44 -0400

    Original files

---
+++ fycirclemyosotis/manifesto.ini.php
@@ -0,0 +1,17 @@
+<?php
+/**
+
+[Manifesto]
+Name="FY Circle Myosotis"
+Depends=Legacy_RenderSystem
+Url="http://www.theme4u.net/"
+Version="Update: 2006/01/15"
+
+[Theme]
+Author=Broom
+ScreenShot="screenshot.png"
+Description="FY Circle Series - Myosotis"
+W3C=OK
+
+*/
+?>
